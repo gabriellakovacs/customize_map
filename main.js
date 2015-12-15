995c90db29784724e26f24fcc75d05d2b8af99a3
gabriella kovacs <kovacsxgabriella@gmail.com>
2015-12-15 01:31:43 +0000

    customize menu displays the areas, that it is going to change on the map while hovering over the button

--- main.js
+++ main.js
@@ -4,25 +4,26 @@
  	var inputfieldZoom = document.getElementById('zoom');
  	var inputfieldLat = document.getElementById('lat');
  	var inputfieldLng = document.getElementById('lng');
+ 	var inputfieldS = document.getElementById('S');
+ 	var mainAreaList = document.getElementsByClassName("mainArea");
 
 
  	
     var mapCanvas = document.getElementById('map');
     var mapOptions = {
         center: new google.maps.LatLng(47.5403, 19.0463),
-        zoom: 4,
+        zoom: 5,
 		mapTypeId: google.maps.MapTypeId.ROADMAP //ROADMAP, SATELLITE, HYBRID, or TERRAIN
 	}
 	var map = new google.maps.Map(mapCanvas, mapOptions);
 
-	map.set('styles', [
-	{
-	featureType: 'road', //administrative, landscape, poi, road, transit, water
-	elementType: 'geometry', //geometry(.icon, .text), labels(.stroke, .fill), all 
-	stylers: [
-	{ color: '#FF7700' }//weight, visibility, color, hue, lightness, saturation
-
-	]
+	var originalStyle = [
+		{
+		featureType: 'road', //administrative, landscape, poi, road, transit, water
+		elementType: 'geometry', //geometry(.icon, .text), labels(.stroke, .fill), all 
+		stylers: [
+		{ color: '#FF7700' }//weight, visibility, color, hue, lightness, saturation
+		]
 	}, {
 	    featureType: 'water',
 	    elementType: 'geometry',
@@ -44,9 +45,7 @@
 	    { lightness: -15 },
 	    { saturation: 50 }
 	    ]
-	},
-
-	{
+	}, {
 	    featureType: 'all',
 	    elementType: 'labels.text',
 	    stylers: [
@@ -55,14 +54,42 @@
 	    { saturation: 50 }
 	    ]
 	}
-	]);
+	];
+
+	var originalStyle = [
+		{
+	    featureType: 'all',
+	    elementType: 'all',
+	    stylers: [
+	    	{ color: '#bbbbbb' }
+	    ]
+	},
+	{
+	    featureType: 'all',
+	    elementType: 'labels.text.stroke',
+	    stylers: [
+	    	{ visibility: 'off' }
+	    ]
+	},
+	{
+	    featureType: 'administrative',
+	    elementType: 'labels.text.fill',
+	    stylers: [
+	    	{  visibility: 'off' }
+	    ]
+	},  
+	{
+	    featureType: 'water',
+	    elementType: 'all',
+	    stylers: [
+	    	{ color: '#eeeeee' }
+	    ]
+	}
+	];
+
+	map.set('styles', originalStyle);
 var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
 var myLatLng =  new google.maps.LatLng(47.5403, 19.0463);
-var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    icon: iconBase + 'schools_maps.png'
-});
 
 	var mapCSS = document.getElementById("map");
 
@@ -94,6 +121,68 @@ var marker = new google.maps.Marker({
     	map.setCenter(newCenter);
     	marker.setPosition(newCenter);
 	};
+
+	inputfieldS.onclick = function() {
+		var color = document.getElementById("color").value;
+		var mapStyle = {
+			featureType: 'landscape.natural.terrain',
+	    	elementType: 'all',
+			stylers: [
+			{ color: color }
+			]
+		};
+
+		originalStyle.push(mapStyle);
+		map.set('styles', 
+			originalStyle
+		);
+	};
+
+	
+	for (var i=0; i < mainAreaList.length; i++) {
+
+		mainAreaList[i].onmouseover = function() {
+			var mainArea = this.id;
+			
+			if (mainArea == "poi" || mainArea=="transit") {
+				map.setZoom(11);
+			}
+			
+			var mapStyle = {
+				featureType: mainArea,
+		    	elementType: 'all',
+				stylers: [
+				{ color: "#ff6622" },
+				{visibility:'on'}
+				]
+			};
+
+			originalStyle.push(mapStyle);
+			map.set('styles', 
+				originalStyle
+			);
+	    };
+
+	    mainAreaList[i].onmouseleave = function() {
+			originalStyle.pop();
+			map.set('styles', 
+				originalStyle
+			);
+
+			map.setZoom(5);
+	    };
+
+	    mainAreaList[i].onclick = function() {
+			window.alert("finallllly");
+	};
+	}
+
+	
+
+	
+
+
+
 
 	marker = new google.maps.Marker({
         position: new google.maps.LatLng(inputfieldLat.value*1, inputfieldLng.value*1),
